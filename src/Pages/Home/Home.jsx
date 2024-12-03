2a36307dc825f4743341626297a6845790003a67
R4ID3NZ4P <iamxtremesifat@gmail.com>
2024-12-03 17:53:49 +0600

    Recharts.js added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,15 @@
 import { useEffect, useState } from "react";
 import Row from "./__row";
 import classNames from "classnames";
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+} from "recharts";
 
 const Home = () => {
     const [data, setData] = useState([]);
@@ -10,54 +19,91 @@ const Home = () => {
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/posts")
             .then((res) => res.json())
-            .then((res) => setData(res));
+            .then((res) => setData(res.slice(0, 25)));
     }, []);
 
     return (
-        <div>
+        <div className="flex flex-col items-center w-full">
             {/* data display */}
-            <div className="flex justify-end">
-                <div className="stats bg-amber-500 text-primary-content">
-                    <div className="stat">
-                        <div className={classNames("stat-title text-black")}>
-                            Buzzer Status
-                        </div>
-                        <div
-                            className={classNames(
-                                "stat-value text-center",
-                                buzzer ? "text-red-600" : "text-green-500"
-                            )}
+            <div className="flex md:flex-col justify-center items-end my-8 w-full">
+                <div className="flex justify-center items-center w-full">
+                    <div>
+                        {/* line chart */}
+                        <LineChart
+                            width={500}
+                            height={300}
+                            data={data}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
                         >
-                            {buzzer ? "On" : "Off"}
-                        </div>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="title" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line
+                                type="monotone"
+                                dataKey="id"
+                                stroke="#8884d8"
+                            />
+                        </LineChart>
                     </div>
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="stats bg-amber-500 text-primary-content">
+                            <div className="stat">
+                                <div
+                                    className={classNames(
+                                        "stat-title text-black"
+                                    )}
+                                >
+                                    Buzzer Status
+                                </div>
+                                <div
+                                    className={classNames(
+                                        "stat-value text-center",
+                                        buzzer
+                                            ? "text-red-600"
+                                            : "text-green-500"
+                                    )}
+                                >
+                                    {buzzer ? "On" : "Off"}
+                                </div>
+                            </div>
 
-                    <div className="stat">
-                        <div className="stat-title text-black">Fan Status</div>
-                        <div
-                            className={classNames(
-                                "stat-value text-center",
-                                fan ? "text-red-600" : "text-green-500"
-                            )}
-                        >
-                            {fan ? "On" : "Off"}
+                            <div className="stat">
+                                <div className="stat-title text-black">
+                                    Fan Status
+                                </div>
+                                <div
+                                    className={classNames(
+                                        "stat-value text-center",
+                                        fan ? "text-red-600" : "text-green-500"
+                                    )}
+                                >
+                                    {fan ? "On" : "Off"}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="border-0">
+                            <button
+                                className={classNames(
+                                    "btn btn-sm btn-error mt-2",
+                                    fan && buzzer ? "" : "btn-disabled"
+                                )}
+                            >
+                                Disable
+                            </button>
                         </div>
                     </div>
                 </div>
-                    <div className="self-end border-0">
-                        <button
-                            className={classNames(
-                                "btn btn-sm btn-error",
-                                fan && buzzer ? "" : "btn-disabled"
-                            )}
-                        >
-                            Disable
-                        </button>
-                    </div>
             </div>
 
             {/* table */}
-            <div className="overflow-x-auto border border-[#39FF14] rounded-md">
+            <div className="overflow-x-auto border border-cyan-900 rounded-lg my-5 w-full">
                 <table className="table table-zebra">
                     {/* head */}
                     <thead>
